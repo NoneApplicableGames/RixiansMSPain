28df0e79dbbefa43102fcdff65bee863bb954d30
Omar Kamel <tekexplorerm@gmail.com>
2026-07-30 07:23:38 +0300

    Add the phobia toggler manually

---
+++ HideDetailsModCode/Scenes/NPhobiaToggler2.cs
@@ -0,0 +1,11 @@
+using Godot;
+using MegaCrit.Sts2.Core.Nodes.Animation;
+using MegaCrit.Sts2.Core.Saves;
+
+namespace HideDetailsMod.HideDetailsModCode.Scenes;
+
+/// <summary>
+/// Will start/stop the animation from an AnimationPlayer depending on if Phobia Mode is on
+/// Used in instances where the animation can make people uncomfortable (ie the Infection card overlay)
+/// </summary>
+partial class NPhobiaAnimationToggler2 : NPhobiaAnimationToggler;
